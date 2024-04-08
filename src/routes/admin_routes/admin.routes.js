c1da6b684643203a3814276898a06ac5f334e5cf
akki83340876 <133499635+akki83340876@users.noreply.github.com>
2024-04-08 11:11:15 +0530

    admin.routes.js

--- src/routes/admin_routes/admin.routes.js
+++ src/routes/admin_routes/admin.routes.js
@@ -4,9 +4,11 @@ const { authorize } = require("../../middleware/authorization");
 
 
 const { adminRegistration , loginAdmin} = require('../../controller/admin_controller/registration.controller')
+const {  Admin_login} = require("../../controller/admin_controller/admin_login.controller")
 
 router.post('/admin_registration', adminRegistration)
-router.post('/admin_login',loginAdmin); // Done 
+
+router.post('/admin_login',Admin_login);
 
 
 module.exports = router;
